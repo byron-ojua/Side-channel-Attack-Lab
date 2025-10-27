089f7932bd678e5fdcf90eec33bb1a1c61e080a5
Byron Ojua-Nice <byron.ojua@gmail.com>
2025-10-26 17:55:58 -0700

    Part 1 init

---
+++ part-1/part1.js
@@ -0,0 +1,9 @@
+export function checkPin(pin, secret, delayFunction) {
+  for (let i = 0; i < secret.length; i++) {
+    if (pin[i] !== secret[i]) return false;
+
+    // Introduce a delay to simulate timing leak
+    delayFunction();
+  }
+  return true;
+}
